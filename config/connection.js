92864525b09afa139fd8b0e2b4b9f81f54478aec
Kevin <kphyde@gmail.com>
2018-11-16 10:23:34 -0800

    add jawsdb credentials

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 // var Sequelize = require("sequelize");
+var connection;
 
 //////// MySQL ////////////
-var connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-01.cleardb.net",
-  port: 3306,
-  user: "bf0a2e4d7888a8",
-  password: "d932d01b",
-  database: "heroku_6a32f2634e36fa1"
-});
-
-mysql://bf0a2e4d7888a8:d932d01b@us-cdbr-iron-east-01.cleardb.net/heroku_6a32f2634e36fa1?reconnect=true
-
-// ////////// Sequelize ////////////
-// var sequelize = new Sequelize("burgers_db", "root", "", {
-//   host: "localhost",
-//   port: 3306,
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 ////// Make My SQL connection. ///////////
 connection.connect(function(err) {
@@ -37,6 +27,3 @@ connection.connect(function(err) {
 
 // Export My SQL connection for our ORM to use.
 module.exports = connection;
-
-// Export Sequelize connection for our ORM to use.
-// module.exports = sequelize;
